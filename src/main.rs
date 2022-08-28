b9c5aeca13bcfeca18a6f6503f2da07c4f7e05a8
TristanAntonsen <tristan.antonsen@gmail.com>
2022-08-28 11:10:17 -0500

    saving image of new random seeds

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod geometry;
 mod seed;
 use canvas::Canvas;
 use export::save_png;
-use geometry::{Ordered_Polygon, point, Line};
+use geometry::{nearest_pixel};
 use seed::Seeds;
 extern crate voronator;
 use voronator::VoronoiDiagram;
@@ -13,11 +13,18 @@ use voronator::delaunator::Point;
 fn main() {
     const WIDTH: usize = 120;
     const HEIGHT: usize = 120;
+    let WHITE = [1.0, 1.0, 1.0];
+
     let mut canvas = Canvas::new(WIDTH, HEIGHT);
 
     let seeds = Seeds::uniform(&canvas, 100);
-    
+    let mut pixel;
     for s in seeds.coords {
-        println!("{:?}",s)
+        pixel = nearest_pixel(&s);
+        canvas.write_pixel(pixel[0], pixel[1], WHITE);
+        // println!("{:?}",s)
     }
+    
+    save_png("canvas.png", canvas);
+
 }
