27288dd1f25e6823de5258c048c788b0b37c6b05
TristanAntonsen <tristan.antonsen@gmail.com>
2022-08-27 20:35:16 -0500

    generating uniform seeds

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod export;
 mod canvas;
 mod geometry;
+mod seed;
 use canvas::Canvas;
 use export::save_png;
 use geometry::{Ordered_Polygon, point, Line};
-
+use seed::Seeds;
+use rand::Rng;
 fn main() {
-
-    let mut canvas = Canvas::new(120, 120);
+    const WIDTH: usize = 120;
+    const HEIGHT: usize = 120;
+    let mut canvas = Canvas::new(WIDTH, HEIGHT);
     let scale = 10.0;
     let polygon = Ordered_Polygon {
         vertices: vec!(
@@ -17,12 +20,15 @@ fn main() {
             [0.0 * scale, 2.5 * scale],
             )
     };
-    for vertex in polygon.vertices {
-        canvas.write_pixel(vertex[0].round() as usize, vertex[1].round() as usize, [1.0, 1.0, 1.0])
-    }
-    save_png("canvas.png", canvas);
+
 
     let point: point = [0.0,5.0];
+    let seeds = Seeds::uniform(&canvas, 100);
 
-    println!("{:?}",point);    
+    println!("Seeds: {:?}", seeds.coords);    
+
+    for seed in seeds.coords {
+        canvas.write_pixel(seed[0].round() as usize, seed[1].round() as usize, [1.0, 1.0, 1.0])
+    }
+    save_png("canvas.png", canvas);
 }
---
+++ src/seed.rs
@@ -0,0 +1,28 @@
+use crate::Canvas;
+use rand::Rng;
+
+
+pub struct Seeds {
+    pub coords : Vec<[f32; 2]>
+}
+
+impl Seeds {
+    pub fn uniform(canvas: &Canvas, count: usize) -> Self {
+        let width = canvas.pixels[0].len();
+        let height = canvas.pixels.len();
+        let mut seeds = Vec::new();
+        let mut rng = rand::thread_rng();
+        let mut x: f32;
+        let mut y: f32;
+        let mut n = 0;
+        while n < count {
+            x = rng.gen_range(0..width) as f32;
+            y = rng.gen_range(0..height) as f32;
+            seeds.push([x,y]);
+            n += 1
+        }
+        Self {
+            coords: seeds
+        }
+    }
+}
