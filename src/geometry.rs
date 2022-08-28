a09ec96579631c754212eb59271b0e6ed6d9c19b
TristanAntonsen <tristan.antonsen@gmail.com>
2022-08-27 20:15:22 -0500

    nearest pixel function

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,9 +1,15 @@
 pub type point = [f32; 2];
+pub type pixel = [usize; 2];
 
 pub struct Ordered_Polygon {
-    pub vertices: Vec<[f32; 2]>
+    pub vertices: Vec<[f32; 2]>,
 }
 
 pub struct Line {
-    pub points: [point; 2]
+    pub points: [point; 2],
+}
+
+//return the nearest pixel as the floor of each floating point coordinate
+pub fn nearest_pixel(point: &point) -> pixel {
+    [point[0].floor() as usize, point[1].floor() as usize]
 }
