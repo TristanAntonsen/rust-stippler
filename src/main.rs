918be0ffb4f31a684d09ed6395bf0f165e29a87a
TristanAntonsen <tristan.antonsen@gmail.com>
2022-08-30 21:56:07 -0500

    testing raster centroid functions on voronoi cells

--- src/main.rs
+++ src/main.rs
@@ -30,61 +30,53 @@ fn main() {
     //main canvas
     let mut canvas = Canvas::new(WIDTH as usize, HEIGHT as usize);
 
-    // //creating start seeds
-    // let mut seeds = Seeds::uniform(&canvas, 20);
-    // let start_seeds = seeds.clone();
+    //creating start seeds
+    let mut seeds = Seeds::uniform(&canvas, 20);
+    let start_seeds = seeds.clone();
 
-    // //create voronoi diagram
-    // let vor_diagram = voronoi(seeds.coords, WIDTH as f64);
+    //create voronoi diagram
+    let vor_diagram = voronoi(seeds.coords, WIDTH as f64);
 
-    // //faces of diagram
-    // let faces = voronoi::make_polygons(&vor_diagram);
+    //faces of diagram
+    let faces = voronoi::make_polygons(&vor_diagram);
     
-    // //initializing variables
-    // let (mut poly, mut sorted_poly);
-    // let (mut _c, mut cV, mut cR, mut color);
+    //initializing variables
+    let (mut poly, mut sorted_poly);
+    let (mut _c, mut cV, mut cR, mut cW, mut color);
 
     //creating weight array (grayscale)
     let mut weights = Weighted_Canvas::from_image("gradient.png");
 
-    // //looping through voronoi regions
-    // for face in &faces {
-    //     //creating unordered polygon from region
-    //     poly = Unordered_Polygon::from_face(face);
-    //     // sorting ordered polygon
-    //     sorted_poly = poly.sort();
+    //looping through voronoi regions
+    for face in &faces {
+        //creating unordered polygon from region
+        poly = Unordered_Polygon::from_face(face);
+        // sorting ordered polygon
+        sorted_poly = poly.sort();
 
-    //     //creating the vertex centroid of the polygon
-    //     _c = vertex_centroid(&sorted_poly.vertices);
-    //     cV = Point::new(_c[0], _c[1]);
-    //     cR = weighted_polygon_centroid(&sorted_poly, &mut weights);
+        //creating the vertex centroid of the polygon
+        _c = vertex_centroid(&sorted_poly.vertices);
+        cV = Point::new(_c[0], _c[1]);
+        cR = weighted_raster_centroid(&sorted_poly, &mut weights);
+        cW = raster_centroid(&sorted_poly, &mut canvas);
 
-    //     //visualizing polygon region
-    //     color = random_color();
-    //     scanline_rasterize_polygon(&sorted_poly,color, &mut canvas);
+        //visualizing polygon region
+        color = random_color();
+        scanline_rasterize_polygon(&sorted_poly,color, &mut canvas);
 
-    //     //visualizing polygon VERTEX CENTROID in RED
-    //     rasterize_circle(&cV, 4, _RED, &mut canvas);
+        //visualizing polygon VERTEX CENTROID in RED
+        rasterize_circle(&cV, 4, _RED, &mut canvas);
 
-    //     //visualizing polygon RASTER CENTROID in BLUE
-    //     rasterize_circle(&cR, 4, _BLUE, &mut canvas);
-    // }
-    // //visualizing START SEEDS in GREEN
-    // for point in &start_seeds.coords {
-    //     rasterize_circle(&point, 4, _GREEN, &mut canvas)
-    // }
+        //visualizing polygon RASTER CENTROID in BLUE
+        rasterize_circle(&cR, 7, _BLUE, &mut canvas);
 
-    let hex = Ordered_Polygon::ngon([256., 257.], 200.0, 8);
-    scanline_rasterize_polygon(&hex, _GREEN, &mut canvas);
-    let cV = vertex_centroid(&hex.vertices);
-    let cR = weighted_raster_centroid(&hex, &mut weights);
-    // let cR = raster_centroid(&hex, &mut canvas);
-
-    //visualizing polygon RASTER CENTROID in BLUE
-    rasterize_circle(&cR, 6, _BLUE, &mut canvas);
-
-    //visualizing polygon VERTEX CENTROID in RED
-    rasterize_circle(&Point::new(cV[0], cV[1]), 4, _RED, &mut canvas);
+        //visualizing polygon WEIGHTED RASTER CENTROID in BLUE
+        rasterize_circle(&cW, 4, _WHITE, &mut canvas);
+    }
+    //visualizing START SEEDS in GREEN
+    for point in &start_seeds.coords {
+        rasterize_circle(&point, 4, _GREEN, &mut canvas)
+    }
 
     save_png("canvas.png", canvas);
 }
