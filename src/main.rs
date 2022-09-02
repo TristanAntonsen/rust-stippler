1edc06a94aa77a702a7f6398af34e9acc19ffe24
TristanAntonsen <tristan.antonsen@gmail.com>
2022-09-01 22:20:35 -0500

    starting to make image size dynamic

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod geometry;
 mod rasterize;
 mod seed;
 mod relax;
+use std::ops::Mul;
+
 use canvas::{random_color, Canvas, Weighted_Canvas, random_grayscale};
 use export::save_png;
 use geometry::{vertex_centroid, Line, Ordered_Polygon, Unordered_Polygon};
@@ -19,8 +21,8 @@ use crate::rasterize::{raster_centroid, rasterize_line_naive};
 use crate::relax::lloyd_relax;
 // use relax::lloyd_relax;
 
-const WIDTH: i32 =512;
-const HEIGHT: i32 = 512;
+// const WIDTH: i32 =512;
+// const HEIGHT: i32 = 512;
 
 fn main() {
     const _RED: [f32; 3] = [1.0, 0.0, 0.0];
@@ -29,20 +31,25 @@ fn main() {
     const _WHITE: [f32; 3] = [1.0, 1.0, 1.0];
     const _BLACK: [f32; 3] = [0.0, 0.0, 0.0];
 
+    //weight canvas
+    let file_path = "sampling/test_im_2_500.png";
+    let mut weight_canvas = Weighted_Canvas::from_image(file_path);
+    let WIDTH = weight_canvas.pixel_weights[0].len();
+    let HEIGHT = weight_canvas.pixel_weights.len();
+    
     //main canvas
     let mut canvas = Canvas::new(WIDTH as usize, HEIGHT as usize);
     let mut canvas2 = Canvas::new(WIDTH as usize, HEIGHT as usize);
     let mut canvas3 = Canvas::new(WIDTH as usize, HEIGHT as usize);
 
-    //weight canvas
-    let mut weight_canvas = Weighted_Canvas::from_image("blob.png");
+
     //creating start seeds
     // let mut seeds = Seeds::uniform(&canvas, 1000);
-    let mut seeds = Seeds::rejection_sample(&weight_canvas, 150, 0.45);
+    let mut seeds = Seeds::rejection_sample(&weight_canvas, 200, 0.3);
 
     let start_seeds = seeds.clone();
 
-    let relaxed = lloyd_relax(&start_seeds, 30, WIDTH as f64, "blob.png");
+    let relaxed = lloyd_relax(&start_seeds, 30, WIDTH as f64, file_path);
 
     for seed in start_seeds.coords {
         rasterize_circle(&seed, 3, _WHITE, &mut canvas2)
@@ -50,6 +57,14 @@ fn main() {
     for seed in &relaxed {
         rasterize_circle(&seed, 3, _WHITE, &mut canvas3)
     }
+    // let mut scaled;
+    // for seed in &relaxed {
+    //     scaled = Point::new(
+    //         f64::try_from(seed.x).unwrap() * 2.0,
+    //         f64::try_from(seed.y).unwrap() * 2.0
+    //     );
+    //     rasterize_circle(&scaled, 1, _WHITE, &mut canvas3)
+    // }
 
     let vor_diagram = voronoi(relaxed, 1080.);
 
