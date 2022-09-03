11db17e1bd4d215106e087c6688350fcdbe4f2c4
TristanAntonsen <tristan.antonsen@gmail.com>
2022-09-03 08:21:17 -0500

    cleaning up imports

--- src/relax.rs
+++ src/relax.rs
@@ -1,9 +1,9 @@
-use crate::canvas::{Weighted_Canvas, Canvas};
-use crate::geometry::{vertex_centroid, Line, Ordered_Polygon, Unordered_Polygon};
-use crate::{geometry::point, seed::Seeds};
-use crate::{weighted_raster_centroid, rasterize_circle};
-use voronoi::{make_line_segments, make_polygons, voronoi, Point};
-use crate::export::{save_image, visualize_frame};
+use crate::canvas::{Weighted_Canvas};
+use crate::geometry::Unordered_Polygon;
+use crate::seed::Seeds;
+use crate::{weighted_raster_centroid};
+use voronoi::{voronoi, Point};
+use crate::export::{visualize_frame};
 
 
 pub fn lloyd_relax(start_seeds: &Seeds,iterations: u16, width: f64, image_path: &str) -> Vec<Point> {
@@ -32,26 +32,15 @@ pub fn lloyd_relax(start_seeds: &Seeds,iterations: u16, width: f64, image_path: 
 
             //creating the weighted centroid of the polygon
             cR = weighted_raster_centroid(&sorted_poly, &mut weights);
-            // cR = vertex_centroid(&sorted_poly.vertices,);
-            // println!("cR: {:?}",cR);
+
             new_points.push(cR);
-            // new_points.push(Point::new(cR[0],cR[1]));
         }
         seeds.coords = new_points;
 
         // --------------
         // EXPORTING SEQUENCE
         // --------------
-        // let mut canvas = Canvas::solid_color(width as usize, width as usize,[1.0,1.0,1.0]);
-        // let mut file_name = "sequence/".to_string();
-        // for point in &seeds.coords {
-        //     rasterize_circle(&point, 3, [0.0, 0.0, 0.0], &mut canvas)
-        // }
-        
-        // file_name.push_str(&i.to_string());
-        // file_name.push_str(".jpg");
-        // save_image(&file_name[..], canvas);
-        // println!("iteration {}",i);
+
         // visualize_frame(i, &seeds, width as usize, width as usize, 2, [1.0,1.0,1.0], [0.0,0.0,0.0])
     }
 
